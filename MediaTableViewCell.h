07933ec14da33ca2e334c0e8fbe890681043415b
Kerolos <keronakhla@gmail.com>
2015-09-26 15:11:23 -0700

    no message

---
+++ MediaTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  MediaTableViewCell.h
+//  Blocstagram
+//
+//  Created by Kerolos Nakhla on 9/26/15.
+//  Copyright (c) 2015 Bloc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MediaTableViewCell : UITableViewCell
+
+@end
---
+++ MediaTableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  MediaTableViewCell.m
+//  Blocstagram
+//
+//  Created by Kerolos Nakhla on 9/26/15.
+//  Copyright (c) 2015 Bloc. All rights reserved.
+//
+
+#import "MediaTableViewCell.h"
+
+@implementation MediaTableViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
